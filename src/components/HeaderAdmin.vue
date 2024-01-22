dcb42a0b32e9c8ef4c2ce505eafdeff659d462a4
LauSalmon <salmon.laura@laposte.net>
2024-01-22 16:11:15 +0100

    modification lien RS et couleur hover

--- src/components/HeaderAdmin.vue
+++ src/components/HeaderAdmin.vue
@@ -13,11 +13,11 @@ import {ArrowLeftEndOnRectangleIcon} from '@heroicons/vue/24/solid'
             <div class="flex flex-row items-center">
                 <input type="search" class="w-96 rounded" placeholder="Rechercher">
                 <router-link to="">
-                  <MagnifyingGlassIcon class="text-white size-8 hover:text-rose-600"/>    
+                  <MagnifyingGlassIcon class="text-white size-8 hover:text-red-adrar"/>    
                 </router-link>
             </div>
             <router-link to="">
-                <ArrowLeftEndOnRectangleIcon class="text-white size-10 m-5 hover:text-rose-600"/>
+                <ArrowLeftEndOnRectangleIcon class="text-white size-10 m-5 hover:text-red-adrar"/>
             </router-link>
             
 
