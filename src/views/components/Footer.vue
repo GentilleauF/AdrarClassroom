50046716e30a3ac48039f0a8d12df85230fd25b5
Camille-Durand <camille.durand@limayrac.fr>
2024-01-22 12:01:55 +0100

    modification Footer & FormConnexion

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -3,14 +3,14 @@
 </script>
 
 <template>
-    <div class="bg-black-adrar p-4 relative bottom-0">
-        <div class="flex flex-row">
-            <div class="px-8">
+    <div class="bg-black-adrar relative bottom-0 left-0 p-4 w-full">
+        <div class="flex flex-row gap-32">
+            <div class="px-16">
                 <img src="../../assets/icons/logo_adrar.png" alt="Logo Adrar" width="200" class="py-2">
                 <a href="https://fr.linkedin.com/school/adrarnumerique/"><img src="../../assets/icons/logo_linkedin.png" alt="Linkedin" width="50"></a>
             </div>
-            <div class="text-center">
-                <h1 class="text-2xl  text-white">Contacts</h1>
+            <div class="text-center px-96">
+                <h1 class="text-2xl text-white">Contacts</h1>
                 <p class="text-grey-adrar">05 34 31 38 10<br>
                 contact@adrar-numerique.com<br>
                 11 avenue de l'Europe<br>
