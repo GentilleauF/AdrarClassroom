0f7cbd639785e8fb3bc12514857c5e988f6d54b3
Luigi <luigi.joubert@gmail.com>
2024-01-23 11:31:40 +0100

    🐛 fix(CreationCompte.vue): fix indentation and formatting for better readability ✨ feat(CreationCompte.vue): add functionality to create a new stagiaire account and select a session from the available options

--- src/components/AjoutCoursFormulaire.vue
+++ src/components/AjoutCoursFormulaire.vue
@@ -1,51 +1,82 @@
+<script setup>
+import { ref } from "vue";
+import { supabase } from "../lib/supabaseClient.js";
+const titre = ref("");
+const contenu = ref("");
+</script>
+
 <template>
-      <form class="space-y-5 flex flex-col p-3 md:p-20 md:w-[60vw]" action="">
-    <div class="flex flex-row justify-center items-center">
-      <label
-        class="w-[20vw] font-medium leading-6 text-gray-900"
-        htmlFor="username"
-      >
-        Titre du cours:
-      </label>
-      <input
-        type="text"
-        required
-        class="rounded border flex-auto border-gray-300 bg-white px-3.5 py-2 text-black  sm:text-sm sm:leading-6"
-        id="username"
-        name="username"
-      />
-    </div>
+    <div class="space-y-5 flex flex-col p-3 md:p-20 md:w-[60vw]">
+        <div class="flex flex-row justify-center items-center">
+            <label
+                class="w-[20vw] font-medium leading-6 text-gray-900"
+				htmlFor="titre"
+				>
+                Titre du cours:
+            </label>
+            <input
+                type="text"
+                class="rounded border flex-auto border-gray-300 bg-white px-3.5 py-2 text-black sm:text-sm sm:leading-6"
+                id="titre"
+                name="titre"
+				v-model="titre"
+				required
+            />
+        </div>
 
-    <div class="flex flex-row justify-center items-center">
-      <label class="w-[20vw] font-medium leading-6 text-gray-900" htmlFor="img">
-        Image du cours :
-      </label>
-      <input
-        type="file"
-        required
-        class="min-w-0 flex-auto rounded border border-gray-300 bg-white px-3.5 py-2 text-black shadow-sm  sm:text-sm sm:leading-6"
-        id="img"
-        name="img"
-      />
-    </div>
+        <div class="flex flex-row justify-center items-center">
+            <label
+                class="w-[20vw] font-medium leading-6 text-gray-900"
+                htmlFor="img"
+            >
+                Image du cours :
+            </label>
+            <input
+                type="file"
+                class="min-w-0 flex-auto rounded border border-gray-300 bg-white px-3.5 py-2 text-black shadow-sm sm:text-sm sm:leading-6"
+                id="img"
+                name="img"
+            />
+        </div>
 
-    <div class="flex flex-row justify-center items-center">
-      <label
-        class="w-[20vw] font-medium leading-6 text-gray-900"
-        htmlFor="lesson-content"
-      >
-        Contenu du cours :
-      </label>
-      <textarea
-        name="lesson-content"
-        required
-        class="min-w-0 flex-auto rounded border border-gray-300 bg-white px-3.5 py-2 text-black shadow-sm sm:text-sm sm:leading-6"
-        cols="30"
-        rows="10"
-      ></textarea>
+        <div class="flex flex-row justify-center items-center">
+            <label
+                class="w-[20vw] font-medium leading-6 text-gray-900"
+                htmlFor="contenu"
+            >
+                Contenu du cours :
+            </label>
+            <textarea
+				id="contenu"
+                name="contenu"
+                class="min-w-0 flex-auto rounded border border-gray-300 bg-white px-3.5 py-2 text-black shadow-sm sm:text-sm sm:leading-6"
+                cols="30"
+                rows="10"
+				v-model="contenu"
+				required
+            ></textarea>
+        </div>
+        <div class="flex justify-center">
+            <button
+                type="submit"
+                class="bg-blackBg w-48 text-white p-1 rounded"
+				@click="creerCours"
+            >
+                Envoyer
+            </button>
+        </div>
     </div>
-    <div class="flex justify-center">
-      <button type="submit" class="bg-blackBg w-48 text-white p-1 rounded">Envoyer</button>
-    </div>
-  </form>
 </template>
+
+<script>
+async function creerCours() {
+	const { data, error } = await supabase
+		.from("cours")
+		.insert([{ title: titre.value, contenu: contenu.value }]);
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(data);
+	}
+}
+</script>
--- src/components/HeaderStagiaire.vue
+++ src/components/HeaderStagiaire.vue
@@ -1,27 +1,53 @@
 <script setup>
-import {ArrowLeftEndOnRectangleIcon} from '@heroicons/vue/24/solid'
-import { RouterLink } from 'vue-router';
+import { ArrowLeftEndOnRectangleIcon } from "@heroicons/vue/24/solid";
+import { RouterLink } from "vue-router";
+import { supabase } from "../lib/supabaseClient.js";
 </script>
 
 <template>
     <header class="bg-black">
         <div class="flex flex-row items-center justify-between">
             <div class="flex flex-row items-center">
-                <img class="size-24 p-2" src="../assets/images/logo-AdrarClassroom.png" alt="logo adrar classroom"/>
+                <img
+                    class="size-24 p-2"
+                    src="../assets/images/logo-AdrarClassroom.png"
+                    alt="logo adrar classroom"
+                />
                 <p class="text-white">ADRAR ClassRoom</p>
             </div>
             <nav class="text-white space-x-10 text-xl">
-                <RouterLink to="" class="hover:text-red-adrar">Cours</RouterLink>
-                <RouterLink to="/blog" class="hover:text-red-adrar">Blog</RouterLink>
-                <RouterLink to="" class="hover:text-red-adrar">Stagiaires</RouterLink>
-                <RouterLink to="/profil" class="hover:text-red-adrar">Mon Espace</RouterLink>
+                <RouterLink to="" class="hover:text-red-adrar"
+                    >Cours</RouterLink
+                >
+                <RouterLink to="/blog" class="hover:text-red-adrar"
+                    >Blog</RouterLink
+                >
+                <RouterLink to="" class="hover:text-red-adrar"
+                    >Stagiaires</RouterLink
+                >
+                <RouterLink to="/profil" class="hover:text-red-adrar"
+                    >Mon Espace</RouterLink
+                >
             </nav>
             <router-link to="">
-                <ArrowLeftEndOnRectangleIcon class="text-white size-10 m-5 hover:text-rose-600"/>
+                <ArrowLeftEndOnRectangleIcon
+                    class="text-white size-10 m-5 hover:text-rose-600"
+                    @click="logout"
+                />
             </router-link>
-            
-
         </div>
-
     </header>
 </template>
+
+<script>
+// Logout
+async function logout() {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("ByeBye (logged out)");
+    }
+}
+</script>
